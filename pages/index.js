7ced629f69385e5c3658fa6c94feae10f948b223
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-06-08 20:45:08 -0700

    events

--- pages/index.js
+++ pages/index.js
@@ -5,53 +5,76 @@ import { useState } from 'react'
 
 export default function Home() {
 
-  const[question, setQuestion] = useState('cookies')
+  const [cookies, cstands] = useState('');
+
+  function submitHandler(event) {
+    event.preventdefault();
+    cstands(event.target.cookies.value)
+    event.target().reset();
+  }
 
   return (
     <div className='bg-black-500'>
-      <Header/>
-      <main className='bg-green-200 py-6 px-8'>
-        <Form onSubmit={submitHandler} className='flex flex-col items-center'/>
+      <Header />
+      <main className='bg-white py-6 px-8'>
+        <Form onSubmit={submitHandler}/>
+        <Response cookies={cookies} className='py-6 px-8'/>
+        <Cookie_Stands className='py-6 px-8' cstands={cstands}/>
+        <p className='py-6 px-8'>Report Table coming Soon...</p>
       </main>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
 
 
-function Header(){
-  return <header className='bg-green-500 py-6 px-8'>
-    <h1>
+function Header() {
+  return <header className='bg-green-600 py-6 px-8'>
+    <h1 className='text-4xl py-.5 px-.5'>
       Cookie Stand Admin
     </h1>
 
   </header>
 }
 
-function Footer({ copywright }){
-  return <footer className='bg-green-500 py-6 px-8'>
-    <p>&copy; {copywright="2022"}</p>
+function Footer({ copywright }) {
+  return <footer className='bg-green-600 py-6 px-8'>
+    <p>&copy;{copywright = "2022"}</p>
   </footer>
 }
 
-function Form(props){
-  return <form onSubmit={props.onSubmit}>
+function Form(props) {
+  return (<form onSubmit={props.onSubmit}  className='flex flex-col items-left bg-green-300'>
     <h2>Create Cookie Stand</h2>
-    <p>Location</p>
-      <input placeholder='location name' required/>
-    <p>Minimum Customer per Hour</p>
-    <input placeholder='Min Cust PH' required/>
-    <p>Maximum Customer per Hour</p>
-    <input placeholder='Maxi Cust PH' required/>
-    <p>Average Cookies per Sale</p>
-    <input placeholder='Avg Cookies per Sale' required/>
-    <button className='px-4 py-30 bg-blue-300'>Create</button>
-    <p>Report Table coming Soon...</p>
-    </form>
+  <div name='cookies'>
+    <div className='grid grid-cols-2 px-4 py-100 pb-4'>
+      <p>Location</p>
+      <input  placeholder='location name' required className='pr-4'/>
+      <p></p>
+    </div>
+
+    <div className='grid gap-4 grid-cols-4 grid-rows-2 px-4 pb-4'>
+      <p>Minimum Customer per Hour</p>
+      <p>Maximum Customer per Hour</p>
+      <p>Average Cookies per Sale</p>
+      <p></p>
+      <input placeholder='Min Cust PH' required />
+      <input placeholder='Maxi Cust PH' required />
+      <input placeholder='Avg Cookies per Sale' required />
+    </div>
+      <button className='px-4 py-300 bg-blue-300'>Create</button>
+    </div>
+  </form>)
 }
 
-function submitHandler(event){
-  event.preventdefault();
-  {/* <p>"{"location":"Barcelona", "minCustomers":2, "maxCustomers":4, "avgCookies":2.5}"</p> */}
-  alert("Submitted")
+function Response(props) {
+  return (
+  <div className='flex items-center justify-center'>{props.cookies}</div>)
+}
+
+function Cookie_Stands(props) {
+  return (
+    <div className='flex items-center justify-center'>{props.cstands}</div>)
+  
+  
 }
